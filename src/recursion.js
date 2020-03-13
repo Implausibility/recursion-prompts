7538ffea345695c5706ba6c083ac58db6cdc8970
Benjamin Barnett <ben.barnett@gmail.com>
2020-03-13 15:36:20 -0500

    Implement 10: Palindrome
    I originally understood 'ignore spaces and capital letters' to mean 'remove
    them,' making the problem unnecessarily complicated with .replace(). Ignoring
    case and leaving whitespace where it lies  is far more straightforward.

--- src/recursion.js
+++ src/recursion.js
@@ -189,6 +189,15 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  let cleanInput = string.toLowerCase();
+  cleanInput = cleanInput.split('');
+  if (cleanInput.length === 0 || cleanInput.length === 1) {
+    return true;
+  }
+  if (cleanInput.shift() !== cleanInput.pop()) {
+    return false;
+  }
+  return palindrome(cleanInput.join(''));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
